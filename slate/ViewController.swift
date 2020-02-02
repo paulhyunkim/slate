a8cd7d1fd128fe859e21d7ba933282e8bc356a5f
Paul Kim <paulhyunkim@gmail.com>
2020-02-02 16:00:06 -0500

    Initial Commit

---
+++ slate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slate
+//
+//  Created by Paul Kim on 2/2/20.
+//  Copyright © 2020 Paul Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
